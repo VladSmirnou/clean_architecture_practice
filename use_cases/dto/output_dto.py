d1f2d46f22dec46a48b741e4cd48fdb3db5efeb5
VladSmirnou <smr.vladislav@gmail.com>
2023-11-19 18:50:59 +0300

    feat: make the output dto generic

--- use_cases/dto/output_dto.py
+++ use_cases/dto/output_dto.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
+from typing import TypeVar, Generic
 
 
-@dataclass
-class OutputDto:
-    output: int | dict
+T = TypeVar("T")
+
+
+@dataclass(frozen=True)
+class OutputDto(Generic[T]):
+    output: T
