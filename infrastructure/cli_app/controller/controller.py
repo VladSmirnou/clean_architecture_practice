b8cfadcdc0d1185845e5714bc618a846fc00977a
VladSmirnou <smr.vladislav@gmail.com>
2023-11-19 15:53:30 +0300

    feat: change the way how the contr accepts use case

--- infrastructure/cli_app/controller/controller.py
+++ infrastructure/cli_app/controller/controller.py
@@ -1,20 +1,14 @@
-from use_cases.use_case import UseCase
 from use_cases.dto.input_dto import InputDto, InputDtoRetrieve
-from use_cases.interfaces.cli_repo_interface import I_InMemoryRepository
-from use_cases.interfaces.cli_presenter_interface import IOutputBoundary
-from infrastructure.cli_app.interfaces.controller_interface import ICliController
+from infrastructure.cli_app.interfaces.controller_interface import \
+    ICliController
+from use_cases.interfaces.use_case_interface import I_InputBoundary
 
 
 class CliController(ICliController):
-    use_case_obj: UseCase
+    use_case_obj: I_InputBoundary
 
-    def __init__(self,
-                 presenter: IOutputBoundary,
-                 repository: I_InMemoryRepository) -> None:
-        self.use_case_obj = UseCase(
-            presenter=presenter,
-            repository=repository
-        )
+    def __init__(self, use_case: I_InputBoundary) -> None:
+        self.use_case_obj = use_case
 
     def process_data(self, data: dict) -> str:
         match data:
