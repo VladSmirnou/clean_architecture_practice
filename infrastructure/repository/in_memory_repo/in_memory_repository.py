2878c8ce3f5df3f6c7367ce800eba29e431c7233
VladSmirnou <smr.vladislav@gmail.com>
2023-11-19 19:49:41 +0300

    feat: in_mem_repo data process mech change

---
+++ infrastructure/repository/in_memory_repo/in_memory_repository.py
@@ -0,0 +1,17 @@
+from use_cases.interfaces.repo_interface import IRepository
+from infrastructure.repository.interfaces.in_memory_db_connector_interface import \
+    I_InMemoryDbConnector
+
+
+class InMemoryRepository(IRepository):
+    def __init__(self, db_connector: I_InMemoryDbConnector) -> None:
+        self.db_connector = db_connector
+
+    def get_saved_calculations(self, amount: int, flag: str) -> list:
+        return self.db_connector.execute(
+            'select * from calculations',
+            params={'amount': amount, 'flag': flag}
+        )
+
+    def save_calculation(self, data: dict) -> None:
+        self.db_connector.execute('insert into calculations', params=data)
