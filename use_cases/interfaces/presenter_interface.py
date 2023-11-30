8893b7e2aa3a26e175bcac15145d8c093d1bf621
VladSmirnou <smr.vladislav@gmail.com>
2023-11-30 21:34:51 +0300

    refactor: rename output boundary interface

--- use_cases/interfaces/presenter_interface.py
+++ use_cases/interfaces/presenter_interface.py
@@ -2,6 +2,6 @@ from abc import ABC, abstractmethod
 from use_cases.dto.output_dto import OutputDto
 
 
-class IOutputBoundary(ABC):
+class OutputBoundaryInterface(ABC):
     @abstractmethod
     def present(self, output_dto: OutputDto) -> dict: ...
