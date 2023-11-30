8b282cbaf20496d9755b8f9737750e0cf54b3cdd
VladSmirnou <smr.vladislav@gmail.com>
2023-11-30 20:51:01 +0300

    feat: remove contr creation from the module

--- flask_main.py
+++ flask_main.py
@@ -1,45 +1,6 @@
-import atexit
-from use_cases.use_case import UseCase
 from presentation.flask_app.flask_app import init_app
-from infrastructure.flask_app.controller.controller import FlaskController
-from infrastructure.flask_app.presenter.presenter import FlaskPresenter
-from infrastructure.repository.postgres_repo.postgres_repository import \
-    PostgresRepository
-from presentation.db.postgres_db.postgres_db_connector import \
-    PostgresDbConnector
-from presentation.db.postgres_db.postgres_db import SetupPostgresDb
-from infrastructure.repository.sql_alc_repo.sql_alc_repo import SqlAlcRepository
-
-
-def handle_script_exit():
-    SetupPostgresDb().close_resourses()
 
 
 if __name__ == '__main__':
-    # ------- SQL alchemy && Postgres repository -------
-    flask_calculator_app = init_app(
-        controller=FlaskController(
-            use_case=UseCase(
-                presenter=FlaskPresenter(),
-                repository=SqlAlcRepository()
-            )
-        )
-    )
-    # -------------------------------------
-
-    # ------- Postgres repository -------
-    flask_calculator_app = init_app(
-        controller=FlaskController(
-            use_case=UseCase(
-                presenter=FlaskPresenter(),
-                repository=PostgresRepository(
-                    db_connector=PostgresDbConnector(
-                        postgres_instanse=SetupPostgresDb()
-                    )
-                )
-            )
-        )
-    )
-    atexit.register(handle_script_exit)
-    # -------------------------------------
+    flask_calculator_app = init_app()
     flask_calculator_app.run(debug=True)
