c489f4e6c06c58fc91312e09a5124684a365528e
VladSmirnou <smr.vladislav@gmail.com>
2023-11-30 22:12:35 +0300

    feat: split the controller into different classes

---
+++ infrastructure/cli_app/controllers/get_saved_result_controller.py
@@ -0,0 +1,18 @@
+from use_cases.dto.input_dto import InputDtoRetrieve
+from use_cases.interfaces.get_saved_results_interface import \
+    GetSavedResultInterface
+from infrastructure.cli_app.interfaces.\
+    get_saved_result_controller_interface import \
+    GetSavedResultControllerInterface
+
+
+class GetSavedResultController(GetSavedResultControllerInterface):
+    use_case_obj: GetSavedResultInterface
+
+    def __init__(self, use_case: GetSavedResultInterface) -> None:
+        self.use_case_obj = use_case
+
+    def process_data(self, data: dict) -> dict:
+        return self.use_case_obj.retrieve_prev_calculations(
+                input_dto=InputDtoRetrieve.from_dict(data)
+            )
