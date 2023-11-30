6be3614debb9cb6dba212e89b6be1221de21dc25
VladSmirnou <smr.vladislav@gmail.com>
2023-11-30 21:05:41 +0300

    feat: add a date field to the OutputDto

--- use_cases/dto/output_dto.py
+++ use_cases/dto/output_dto.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import TypeVar, Generic
+import datetime
 
 
 T = TypeVar("T")
@@ -8,3 +9,4 @@ T = TypeVar("T")
 @dataclass(frozen=True)
 class OutputDto(Generic[T]):
     output: T
+    date: datetime.date = datetime.date.today()
